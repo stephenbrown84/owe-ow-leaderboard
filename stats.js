ea39cfd48bba68dc81dd764c2cc98cd5d59a7974
Brown <STEPHEN.BROWN@HRSDC-RHDCC.GC.CA>
2017-03-02 13:08:11 -0330

    test

--- stats.js
+++ stats.js
@@ -15,9 +15,9 @@ Stats.prototype.addPlayerStats = function (friendlyName, data) {
 }
 
 Stats.prototype.compare = function (player1, player2, hero) {
-    if (this.allStats[player1].quickplay.heroes[hero].eliminations > this.allStats[player2].quickplay.heroes[hero].eliminations)
+    if (this.allStats[player1].quickplay.heroes[hero].eliminations_per_life > this.allStats[player2].quickplay.heroes[hero].eliminations_per_life)
         return 1;
-    else if (this.allStats[player1].quickplay.heroes[hero].eliminations < this.allStats[player2].quickplay.heroes[hero].eliminations)
+    else if (this.allStats[player1].quickplay.heroes[hero].eliminations_per_life < this.allStats[player2].quickplay.heroes[hero].eliminations_per_life)
         return -1;
     return 0;
 }
