9b7d2486341da90c7b6c4f935cd34557a3ae414b
Brown <STEPHEN.BROWN@HRSDC-RHDCC.GC.CA>
2017-03-02 12:20:39 -0330

    test

--- stats.js
+++ stats.js
@@ -1,18 +1,22 @@
 'use strict';
 
-module.exports = function () {
+module.exports = Stats;
+
+function Stats() {
     //const BATTLE_TAGS = ['Zaralus-1670', 'Nuuga-1351', 'MegaArcon-1653'];
     //const HERO_NAMES = ['pharah', 'reaper', 'soldier:_76'];
 
     this.allStats = {};
 
-    this.addPlayerStats = function (friendlyName, data) {
-        console.dir(data);
-        this.allStats[friendlyName] = data;
-    }
+}
 
-    this.compare = function (player1, player2, hero) {
-        return this.allStats;
+Stats.prototype.addPlayerStats = function (friendlyName, data) {
+    console.dir(data);
+    this.allStats[friendlyName] = data;
+}
+
+Stats.prototype.compare = function (player1, player2, hero) {
+    return this.allStats;
         /*
         if (this.allStats[player1].competitive.heroes[hero].eliminations > this.allStats[player2].competitive.heroes[hero].eliminations)
             return 1;
@@ -20,5 +24,5 @@ module.exports = function () {
             return -1;
         return 0;
         */
-    }
 }
+
