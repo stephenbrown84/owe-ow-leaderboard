917b85e485bb885e244923d5b3b028ca01bfa73b
Brown <STEPHEN.BROWN@HRSDC-RHDCC.GC.CA>
2017-03-02 16:11:43 -0330

    test

--- stats.js
+++ stats.js
@@ -43,7 +43,7 @@ Stats.prototype.compare = function (player1, player2, hero) {
     var total_ratio = (elimn_per_life_ratio + weapon_accuracy_ratio + solo_kills_ratio + objective_kills_ratio) / 4.0;
 
     console.log(player1);
-    console.log(palyer2);
+    console.log(player2);
     console.log(total_ratio.toString());
 
     if (total_ratio > 0.5)
