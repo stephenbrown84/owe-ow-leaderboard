3860be75b36ae7d589eef89451b9f14c8f8b68d0
Brown <STEPHEN.BROWN@HRSDC-RHDCC.GC.CA>
2017-03-02 11:56:25 -0330

    test

--- stats.js
+++ stats.js
@@ -11,9 +11,9 @@ module.exports = function () {
     }
 
     this.compare = function (player1, player2, hero) {
-        if (allStats[player1].competitive.heroes[hero].eliminations > allStats[player2].competitive.heroes[hero].eliminations)
+        if (this.allStats[player1].competitive.heroes[hero].eliminations > this.allStats[player2].competitive.heroes[hero].eliminations)
             return 1;
-        else if (allStats[player1].competitive.heroes[hero].eliminations < allStats[player2].competitive.heroes[hero].eliminations)
+        else if (this.allStats[player1].competitive.heroes[hero].eliminations < this.allStats[player2].competitive.heroes[hero].eliminations)
             return -1;
         return 0;
     }
