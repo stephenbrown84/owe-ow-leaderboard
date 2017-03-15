cfee2bb251190cf7e569462c7d47473370590e5d
Brown <STEPHEN.BROWN@HRSDC-RHDCC.GC.CA>
2017-03-15 12:41:11 -0230

    Beginning to make site moer mobile friendly

--- public/core.js
+++ public/core.js
@@ -502,4 +502,13 @@ angular.module("app", ["googlechart", "rzModule", 'ui.bootstrap', 'ngSanitize'])
         };
     }
 
-});
+})
+.filter('range', function () {
+    return function (input, min, max) {
+        min = parseInt(min); //Make string input int
+        max = parseInt(max);
+        for (var i = min; i < max; i++)
+            input.push(i);
+        return input;
+    };
+});;
