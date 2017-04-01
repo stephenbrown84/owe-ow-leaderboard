988b5a4895d138d784092560f1bcd81daf9dcaae
arien12 <arien12@gmail.com>
2017-04-01 20:59:10 -0230

    Added Steph to list of battle tags

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 const BATTLE_TAGS = ['NorthernYeti-1308', 'MegaArcon-1653', 'noj-1818', 'Nuuga-1351', 'Zaralus-1670', 'Nemisari-1767',
-    'Isoulle-1235', 'Lawbringer-11174', 'Nick-15366', 'Dirtnapper-1628', 'Suracis-1355', 'WiseOldGamer-1346', 'Jay-11736'] //, 'Chesley-1524'];
+    'Isoulle-1235', 'Lawbringer-11174', 'Nick-15366', 'Dirtnapper-1628', 'Suracis-1355', 'WiseOldGamer-1346', 'Jay-11736', 'StephyCakes-1653'] //, 'Chesley-1524'];
 const HERO_NAMES = ['pharah', 'reaper', 'soldier76', 'reinhardt', 'junkrat', 'mei', 'tracer', 'genji', 'mccree', 'winston',
     'roadhog', 'zenyatta', 'mercy', 'ana', 'sombra', 'bastion', 'hanzo', 'widowmaker', 'dva', 'symmetra', 'zarya', 'orisa'];
 const HERO_NAMES_CLEAN = ['pharah', 'reaper', 'soldier76', 'reinhardt', 'junkrat', 'mei', 'tracer', 'genji', 'mccree', 'winston',
