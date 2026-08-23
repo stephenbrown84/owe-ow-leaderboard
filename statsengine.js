951ec813cfabd48adeb04009297f1c6fde4c46e6
Stephen Brown <stephenbrown84@gmail.com>
2026-08-23 00:04:44 -0230

    Field Addition: Add charged_shot_accuracy (weight 1.25) to Sojourn evaluation block in statsengine.js and update season snapshots

--- statsengine.js
+++ statsengine.js
@@ -535,6 +535,7 @@ function getImportantFieldsFor(hero, playMode) {
             {name: 'final_blows_avg_per_10_min', prettyName: 'Final Blows Per 10 Min', weight: 1.5, required: false},
             {name: 'hero_damage_done_avg_per_10_min', prettyName: 'Hero Damage Per 10 Min', weight: 1.5, required: false},
             {name: 'charged_shot_kills_avg_per_10_min', prettyName: 'Railgun Charged Kills Per 10 Min', weight: 1.25, required: false},
+            {name: 'charged_shot_accuracy', prettyName: 'Railgun Charged Accuracy', weight: 1.25, required: false},
             {name: 'overclock_kills_avg_per_10_min', prettyName: 'Overclock Kills Per 10 Min', weight: 1.2, required: false},
             {name: 'disruptor_shot_kills_avg_per_10_min', prettyName: 'Disruptor Shot Kills Per 10 Min', weight: 1.0, required: false},
             {name: 'critical_hits_avg_per_10_min', prettyName: 'Critical Hits Per 10 Min', weight: 1.0, required: false}
