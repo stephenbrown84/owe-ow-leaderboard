d4c19c39206ab60a58d70245e0999681543c8682
Stephen Brown <stephenbrown84@gmail.com>
2026-08-22 23:27:01 -0230

    Data & Engine Reset: Remove Jamie-1389 from BATTLE_TAGS and stats_backup, restore hasEnoughTimePlayed threshold in statsengine.js back to > 19 mins, and regenerate season snapshots

--- statsengine.js
+++ statsengine.js
@@ -579,7 +579,7 @@ function getRequiredFieldsFor(hero, playMode) {
 }
 
 function hasEnoughTimePlayed(heroStats, hero) {
-    return ('time_played' in heroStats) && (getAttr(heroStats, 'time_played') >= 1);
+    return ('time_played' in heroStats) && (getAttr(heroStats, 'time_played') > 19);
 }
 
 function hasRequiredFieldsForHero(heroStats, hero, playMode) {
