cd9b26f0541c383e691493fcf56d6eb6ff96b640
Stephen Brown <stephenbrown84@gmail.com>
2026-08-23 00:26:53 -0230

    Weight Adjustment: Increase weight of barrier_damage_done_avg_per_10_min to 1.25 for Symmetra in statsengine.js

--- statsengine.js
+++ statsengine.js
@@ -420,7 +420,7 @@ function getImportantFieldsFor(hero, playMode) {
             {name: 'eliminations_avg_per_10_min', prettyName: 'Eliminations Per 10 Min', weight: 1.0, required: false},
             {name: 'final_blows_avg_per_10_min', prettyName: 'Final Blows Per 10 Min', weight: 1.5, required: false},
             {name: 'hero_damage_done_avg_per_10_min', prettyName: 'Hero Damage Per 10 Min', weight: 1.5, required: false},
-            {name: 'barrier_damage_done_avg_per_10_min', prettyName: 'Barrier Damage Per 10 Min', weight: 0.5, required: false},
+            {name: 'barrier_damage_done_avg_per_10_min', prettyName: 'Barrier Damage Per 10 Min', weight: 1.25, required: false},
             {name: 'sentry_turret_kills_avg_per_10_min', prettyName: 'Sentry Turret Kills Per 10 Min', weight: 1.0, required: false},
             {name: 'secondary_direct_hits_avg_per_10_min', prettyName: 'Alt-Fire Direct Hits Per 10 Min', weight: 1.25, required: false},
             {name: 'players_teleported_avg_per_10_min', prettyName: 'Players Teleported Per 10 Min', weight: 0.5, required: false}
